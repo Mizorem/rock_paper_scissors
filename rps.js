f551e8db5733415f0c0d0e2f210261c9fd5311ec
Mizore <honeydewgreenteawithtapioca@gmail.com>
2022-09-27 00:22:56 -0500

    made a rock paper scissor, now trying to make a loop

--- rps.js
+++ rps.js
@@ -1,3 +1,17 @@
-function getComputerChoice{
-    
+function getComputerChoice() {
+    let choice = [
+        'rock',
+        'paper',
+        'scissor'];
+    console.log(Math.floor(Math.random()*choice.length));
 }
+
+function singleround(playerSelection, computerSelection) {
+    for rock
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+function game()
