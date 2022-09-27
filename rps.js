87013c07d7936b35658eaa70cc401c7d1a572250
Mizore <honeydewgreenteawithtapioca@gmail.com>
2022-09-27 02:19:13 -0500

    Fixed the loops

--- rps.js
+++ rps.js
@@ -1,17 +1,54 @@
+//start with 0 score//
+let playerScore = 0;
+let computerScore = 0;
+
+//computer returning choice//
 function getComputerChoice() {
     let choice = [
         'rock',
         'paper',
-        'scissor'];
-    console.log(Math.floor(Math.random()*choice.length));
+        'scissors'];
+
+    let choicers = choice[Math.floor(Math.random()*choice.length)];
+    return choicers;
 }
 
-function singleround(playerSelection, computerSelection) {
-    for rock
-}
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You chose paper and I chose scissors :) , You Lose!" ;
+        computerScore+=1;
+    } else if (playerSelection == "paper" && computerSelection =="rock") {
+        return "You chose paper and I chose rock ): , You Win!";
+        playerScore+=1;
+    } else if (playerSelection == "rock" && computerSelection =="scissors") {
+        return "You chose rock and I chose scissors ): , You Win!";
+        playerScore+=1;
+    } else if (playerSelection == "rock" && computerSelection =="paper") {
+        return "You chose rock and I chose paper :), You Lose!";
+        computerScore+=1;
+    } else if (playerSelection == "scissors" && computerSelection =="rock") {
+        return "You chose scissors and I chose rock :) , You Lose!";
+        computerScore+=1;
+    } else if (playerSelection == "scissors" && computerSelection =="paper") {
+        return "You chose scissors and I chose paper ): , You Win!";
+        playerScore+=1;
+    } else {
+        return "It's a Draw?";
+    }
+    };
 
-const playerSelection = "rock";
+for (let i=0; i<5; i++) {
+const playerSelection = prompt("Rock, Paper, or Scissors?");
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+};
 
-function game()
+//5 rounds//
+
+    for(let i=0; i<5; i++) { {
+        if (playerScore > computerScore) {
+            console.log("You are a victor!");
+        } else if (playerscore=computerScore) { console.log("We tied!")
+        } else {console.log("You are a loser!")}
+    }
+};
